22cc22b45ad521822118dc8b61527801a8b4ad39
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2025-07-01 13:02:49 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,6 @@ const loginUser = async (req, res) => {
       secure: true,
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
-      domain: '/',
     });
       const message = 'Login successful';
 
