d2e30ebf1421a4a33992cad4f96609c32bb515f1
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2026-02-11 14:00:20 +0530

    check cicd 2
    Updated test endpoint message for CI/CD context.

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use('/api/user', userRoutes);
 app.use('/api/feedback', feedbackRoutes);
 app.use('/api/interview', interviewRoutes);
 app.get('/test',(req,res)=>{
-  console.log("Everything is working fine!");
-  res.send("Everything is working fine!");
+  console.log("Everything is working fine! done with cicd");
+  res.send("Everything is working fine! done with cicd");
 })
 app.use((err, req, res, next) => {
   if (res.headersSent) return next(err);
@@ -49,5 +49,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
+
 
 
